a083f7c041101dc59a043585ade9fa3550572b2f
haixiangyan <haixiang6123@gmail.com>
2022-01-07 02:03:49 +0800

    feat: 添加 stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,15 +8,17 @@ module.exports = {
     "eslint:recommended", // eslint 自己的推荐规则，最佳实践最小集
     "plugin:@typescript-eslint/recommended", // typescript-eslint 的推荐规则，只是这些最佳规则都是针对 TS 的
     "plugin:@typescript-eslint/recommended-requiring-type-checking", // tsconfig.json 里 Type Checking 的推荐规则
+    "plugin:prettier/recommended", // 禁用 eslint 关于代码的风格的规则，使用 prettier 的风格
   ],
   parser: "@typescript-eslint/parser", // 能看懂 TypeScript
   parserOptions: {
     project: ["./tsconfig.json"], // 告诉 eslint：tsconfig 在哪
   },
   plugins: [
-    "@typescript-eslint", // 可以使用一些规则
+    "@typescript-eslint", // 使用 typescript x eslint 的插件
+    "prettier", // 使用 prettier 插件
   ],
   rules: {
-    "max-len": ["error", { "code": 80 }],
+    "prettier/prettier": "error", // 代码风格有问题就报 error
   },
 };
