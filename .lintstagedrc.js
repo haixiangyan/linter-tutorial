cf64815ce3c86b3a6e916b0fb3b37a0cdf2f4d1f
haixiangyan <haixiang6123@gmail.com>
2022-01-10 12:54:17 +0800

    feat: 添加 package.json

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   '**/*.{ts,tsx}': [
-    () => 'tsc -p tsconfig.json --noEmit', // 判断语法问题和类型检查
+    () => 'tsc -p tsconfig.json --noEmit --skipLibCheck', // 判断语法问题和类型检查
     "eslint --cache --fix", // 自动修复代码风格
   ],
   "**/*.{js,jsx}": [
